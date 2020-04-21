489eef7a73699ca85c6af24ab8a987f6e59077b1
U U <u.ulugbek@gmail.com>
2020-04-21 22:41:16 +0300

    heroku settings

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 //db config
 const db = require("./config/keys").mongoURI;
